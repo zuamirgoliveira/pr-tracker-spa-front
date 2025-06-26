466bc5b95eb719b6c6682e2627b3d74fa8644939
zuamirgoliveira <zuamirgutemberg@gmail.com>
2025-06-26 00:20:11 -0300

    feat: Remove exempl of files and folders

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+
+const container = document.getElementById('root');
+if (!container) throw new Error('Root container missing in index.html');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
